65e5e8e7a6ba632784a991a1013c565720531504
lvojp <lvojp.sgp@gmail.com>
2018-03-23 07:44:30 +0000

    直接ジャンプによる誤動作を防ぐため、リファラーを見るように変更

--- routes/index.js
+++ routes/index.js
@@ -10,21 +10,43 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/ledon', function(req, res, next) {
-  res.render('index', { title: 'クロネコ集荷依頼' });
-    const exec = require('child_process').exec;
-    exec('./on_led.sh;', (err, stdout, stderr) => {
-        if (err) { console.log(err);  }
-        console.log(stdout);
+  var rf = req.get('Referrer');
+  if(rf === undefined){
+    rf = "led"
+  }
+  
+  if (rf.indexOf("led") === -1){
+    console.log("yosi");
+    res.render('index', { title: 'クロネコ集荷依頼' });
+      const exec = require('child_process').exec;
+      exec('./on_led.sh;', (err, stdout, stderr) => {
+          if (err) { console.log(err);  }
+          console.log(stdout);
     });
+  }else{
+    res.render('index', { title: '不正な処理 > トップに戻る' });
+  }
+
 });
 
 router.get('/ledoff', function(req, res, next) {
-  res.render('index', { title: 'クロネコ集荷依頼' });
-    const exec = require('child_process').exec;
-    exec('./wait_led.sh', (err, stdout, stderr) => {
-        if (err) { console.log(err);  }
-        console.log(stdout);
+  var rf = req.get('Referrer');
+  if(rf === undefined){
+    rf = "led"
+  }
+
+  if (rf.indexOf("led") === -1){
+    console.log("yosi");
+    res.render('index', { title: 'クロネコ集荷依頼' });
+      const exec = require('child_process').exec;
+      exec('./wait_led.sh', (err, stdout, stderr) => {
+          if (err) { console.log(err);  }
+          console.log(stdout);
     });
+  }else{
+    res.render('index', { title: '不正な処理 > トップに戻る' });
+  }
+
 });
 
 module.exports = router;
